987d5c7d92fb2ee614c269cbca8ab1eeeb534028
Hamukaze <maplecow@gmail.com>
2023-12-31 22:26:24 +0800

    Added increase speed

--- Assets/BallLogic.cs
+++ Assets/BallLogic.cs
@@ -7,6 +7,8 @@ public class BallLogic : MonoBehaviour
     public float speed;
     public Rigidbody2D rigidBody;
     private Vector2 direction;
+    private int bounces = 0;
+    private float currentSpeed;
     [SerializeField] private GameObject ball;
     // Start is called before the first frame update
     void Start()
@@ -14,6 +16,7 @@ public class BallLogic : MonoBehaviour
         transform.name = transform.name.Replace("(Clone)", "").Trim();
         direction = RandomVector();
         rigidBody.velocity = direction * speed;
+        currentSpeed = calcCurrSpeed();
     }
 
     // Update is called once per frame
@@ -26,7 +29,7 @@ public class BallLogic : MonoBehaviour
     {
         int[] xDirections = { -1, 1 };
         var x = xDirections[Mathf.RoundToInt(Random.Range(0, xDirections.Length))];
-        var y = Random.Range(-0.95f, 0.95f);
+        var y = Random.Range(-0.90f, 0.90f);
         return new Vector2(x, y);
     }
 
@@ -34,12 +37,27 @@ public class BallLogic : MonoBehaviour
     {
         Vector2 surfaceNormal = collision.contacts[0].normal;
         direction = Vector2.Reflect(direction, surfaceNormal);
-        rigidBody.velocity = direction * (speed + 2);
+        if(collision.gameObject.tag == "paddle")
+        {
+            bounces += 1;
+            currentSpeed = calcCurrSpeed();
+            rigidBody.velocity = direction * calcCurrSpeed();
+        } else
+        {
+            rigidBody.velocity = direction * currentSpeed;
+        }
+        Debug.Log(currentSpeed);
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Instantiate(ball, new Vector3(0, 0, 0), transform.rotation);
         Destroy(gameObject);
+    }
+
+    private float calcCurrSpeed()
+    {
+         return speed + 2 + (bounces * 0.1f);
     }
 }
